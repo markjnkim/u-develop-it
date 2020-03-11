197550df5827d089152a0cc8d820459b7ca39c2f
markjnkim <mark.jn.kim@gmail.com>
2020-03-11 15:09:53 -0700

    schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', TRUE),
+  ('Virginia', 'Woolf', TRUE),
+  ('Piers', 'Gaveston', FALSE),
+  ('Charles', 'LeRoi', TRUE),
+  ('Katherine', 'Mansfield', TRUE),
+  ('Dora', 'Carrington', FALSE),
+  ('Edward', 'Bellamy', FALSE),
+  ('Montague', 'Summers', TRUE),
+  ('Octavia', 'Butler', TRUE),
+  ('Unica', 'Zurn', TRUE);
